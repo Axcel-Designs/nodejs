4cce6cde5c503b2e18189bc9b48494031e3f6b53
x <ackelboffin96@gmail.com>
2026-06-19 13:04:02 +0100

    nestjs todo

--- project_04/src/main.ts
+++ project_04/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 3210;
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo Documentation')
+    .setDescription('A documentation for CRUD Todo Application')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidocs', app, document);
 
   await app.listen(process.env.PORT ?? PORT);
 
